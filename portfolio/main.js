30a8de5fa09e66bd0e64a4892305dd1da617cf55
박수아 <xlalfskdl@naver.com>
2021-02-26 04:16:23 +0900

    [ADD] portfolio scroll event

--- portfolio/main.js
+++ portfolio/main.js
@@ -21,6 +21,7 @@ navbarMenu.addEventListener('click', (event) => {
   }
   navbarMenu.classList.remove('open');
   scrollIntoView(link);
+  selectNavItem(target);
 });
 
 // Navbar toggle button for small screen
@@ -88,7 +89,81 @@ workBtnContainer.addEventListener('click', (e) => {
   }, 300);
 });
 
+
+// 1. 모든 색션 요소들과 메뉴 아이템들을 가지고 온다
+const sectionIds = [
+  '#home',
+  '#about',
+  '#skills',
+  '#work',
+  '#testimonials',
+  '#contact',
+];
+const sections = sectionIds.map(id => document.querySelector(id));
+const navItems = sectionIds.map(id =>
+  document.querySelector(`[data-link="${id}"]`)
+);
+
+let selectedNavIndex = 0;
+let selectedNavItem = navItems[0];
+function selectNavItem(selected) {
+  selectedNavItem.classList.remove('active');
+  selectedNavItem = selected;
+  selectedNavItem.classList.add('active');
+}
+
 function scrollIntoView(selector) {
   const scrollTo = document.querySelector(selector);
   scrollTo.scrollIntoView({ behavior: 'smooth' });
+  selectNavItem(navItems[sectionIds.indexOf(selector)]);
 }
+
+
+// 2.IntersectionObserver를 이용해서 모든 색션들을 관찰한다.
+const observerOptions = {
+  root : null,
+  rootMargin:'0px',
+  threshold:0.3,
+}
+
+const observerCallback = (entries, observer) => {
+  entries.forEach(entry => {
+    // entry가 빠져나갈때 entry의 target은 나가는 선생님한테 초점 맞춤 
+    if (!entry.isIntersecting && entry.intersectionRatio > 0) {
+      const index = sectionIds.indexOf(`#${entry.target.id}`);
+      // (entry.boundingClientRect.y < 0) 스크롤링이 아래로 되어서 페이지가 올라옴
+      if (entry.boundingClientRect.y < 0) {
+        // 이후 index로 지정
+        selectedNavIndex = index + 1;
+      } else {
+        // 이전 index로 지정
+        selectedNavIndex = index - 1;
+      }
+    }
+  });
+};
+
+const observer = new IntersectionObserver(observerCallback,observerOptions);
+
+// 3. 보여지는 색션에 해당하는 메뉴 아이템을 활성화 시킨다.
+sections.forEach(section => observer.observe(section));
+
+// 사용자가 마우스나 트랙패드를 이용해서 스크롤하는 경우는 'wheel'이벤트 발생(스스로 스크롤하는 경우)
+// scroll은 모든 스크롤이 해당하는 이벤트가 발생할때마다 생성되는 이벤트
+window.addEventListener('wheel', () => {
+  if (window.scrollY === 0) {
+    selectedNavIndex = 0;
+  } else if (Math.round(window.scrollY + window.innerHeight) >=  document.body.clientHeight
+  ) {
+    selectedNavIndex = navItems.length -1
+  }
+  selectNavItem(navItems[selectedNavIndex]);
+});
+
+// 여기에서 스크롤 해서 페이지 제일 아래로 내렸을경우, 
+
+// scrollY와 window창의 innerHeight 값을 더한값이 정확하게 일치 하지 않는 경우가 있어요.
+
+// document.body.clientHeight은 1270 일 수 있고,
+
+// scrollY와 window창의 innerHeight 더한값은 1269.2 이렇게 소수점이 나올 수 있기 때문이예요.
